ca562b02d7eb363eb85dafc88ef108f2ae021458
Nikolay Harchuk <nicksoner@yandex.ru>
2025-08-22 01:02:40 +0300

    update homework 22.08 5.2

---
+++ src/test/java/FiveDragDrop.java
@@ -0,0 +1,46 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FiveDragDrop {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+    }
+
+    @Test
+    void Actions() {
+
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        actions().clickAndHold($("#column-a"))
+                .moveToElement($("#column-b"))
+                .release()
+                .perform();
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void dragAndDrop() {
+        // Проверяем начальное состояние
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
+        $("#column-a").dragAndDrop(to("#column-b"));
+
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+    }
