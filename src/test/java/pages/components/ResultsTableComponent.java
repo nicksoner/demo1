402c76c877383ea9e36a53c7f8b5d42bda3c0a93
Nikolay Harchuk <nicksoner@yandex.ru>
2025-09-04 14:44:11 +0300

    update homework 04.09 lesson 6 update1.0

--- src/test/java/pages/components/ResultsTableComponent.java
+++ src/test/java/pages/components/ResultsTableComponent.java
@@ -10,13 +10,6 @@ public class ResultsTableComponent {
 
     private final SelenideElement table = $(".table-responsive");
 
-    public ResultsTableComponent verifyResults(String... values) {
-        for (String value : values) {
-            table.shouldHave(text(value));
-        }
-        return this;
-    }
-
     public void checkResult(String key, String value) {
         table.$(byText(key))
                 .parent()
